5cb052974c42c352fb275aa699086984123487aa
Troy Surrett <troy.surrett@netprophets.com>
2012-10-03 21:22:47 -0400

    Small refactorings
    - extract variable
    - Improved naming of aspell custom dictionary

--- test/helper.rb
+++ test/helper.rb
@@ -20,9 +20,10 @@ end
 class AnonymousUser < User ; end
 
 def check_spelling_in_file(path_relative_to_gem_root)
-  path = "#{File.dirname(__FILE__)}/../#{path_relative_to_gem_root}"
+  here = File.dirname(__FILE__)
+  path = "#{here}/../#{path_relative_to_gem_root}"
   begin
-    aspell = "aspell list --add-extra-dicts=#{File.dirname(__FILE__)}/word_list.rws"
+    aspell = "aspell list --add-extra-dicts=#{here}/sentient_user_excused_word_list.rws"
     aspell_output = `cat #{path} | #{aspell}`
   rescue => err
     warn "You probably don't have aspell. On mac: brew install aspell --lang=en"
